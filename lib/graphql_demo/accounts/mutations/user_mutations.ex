c6d48ef2d510176dde17c7121f3bcf11081c078b
John Thomas Raphael DG Dulay <johnthomasraphael@yahoo.com>
2019-09-30 15:46:29 +0900

    Create account user mutations

---
+++ lib/graphql_demo/accounts/mutations/user_mutations.ex
@@ -0,0 +1,13 @@
+defmodule GraphqlDemo.Accounts.Mutations.UserMutations do
+  use GraphqlDemo, :mutation
+  alias GraphqlDemo.Accounts.User
+
+  def build_user_changeset(%User{} = user, params \\ %{}), do: User.changeset(user, params)
+
+  def create_user_changeset(params), do: build_user_changeset(%User{}, params)
+
+  def update_user_changeset(%User{} = user, params), do: build_user_changeset(user, params)
+
+  def delete_user_changeset(%User{} = user),
+    do: user |> build_user_changeset |> put_change(:archived_at, DateTime.utc_now())
+end
