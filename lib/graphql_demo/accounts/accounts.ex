517f5668b969cffa0ce112c1bef4ec4d66b39209
John Thomas Raphael DG Dulay <johnthomasraphael@yahoo.com>
2019-09-30 15:58:03 +0900

    Create account context

--- lib/graphql_demo/accounts/accounts.ex
+++ lib/graphql_demo/accounts/accounts.ex
@@ -1,1 +1,13 @@
+defmodule GraphqlDemo.Accounts do
+  alias GraphqlDemo.Accounts.Mutations.UserMutations
+  alias GraphqlDemo.Accounts.Queries.UserQueries
+  alias GraphqlDemo.Accounts.Resolvers.UserResolvers
 
+  defdelegate query_users(params \\ %{}), to: UserQueries
+  defdelegate build_user_changeset(user, params \\ %{}), to: UserMutations
+  defdelegate all_users(params \\ %{}), to: UserResolvers
+  defdelegate find_user(params), to: UserResolvers
+  defdelegate create_user(params), to: UserResolvers
+  defdelegate update_user(user, params), to: UserResolvers
+  defdelegate delete_user(user), to: UserResolvers
+end
