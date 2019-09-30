6085805d411ff4d14af2c533d8e2b747ce3bfcea
John Thomas Raphael DG Dulay <johnthomasraphael@yahoo.com>
2019-09-30 16:53:39 +0900

    Create post schema

---
+++ lib/graphql_demo/blog/schemas/post.ex
@@ -0,0 +1,23 @@
+defmodule GraphqlDemo.Blog.Post do
+  use GraphqlDemo, :schema
+
+  schema "posts" do
+    field :title, :string
+    field :content, :string
+    field :archived_at, :utc_datetime
+
+    belongs_to :user, GraphqlDemo.Accounts.User
+
+    timestamps()
+  end
+
+  @required_attrs [:title]
+  @optional_attrs [:content]
+
+  @doc false
+  def changeset(%__MODULE__{} = struct, params \\ %{}) do
+    struct
+    |> cast(params, @required_attrs ++ @optional_attrs)
+    |> validate_required(@required_attrs)
+  end
+end
